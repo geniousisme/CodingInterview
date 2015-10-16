1588c146926331999c489b2f436f788934556bee
chrishsu <chia-hao.hsu@aiesec.net>
2015-10-16 10:57:29 -0400

    add one valid parenthese google - version

--- leetCode/Python2/20-validParenthese.py
+++ leetCode/Python2/20-validParenthese.py
@@ -7,7 +7,7 @@ class Solution(object):
         stack = []
         length = len(s)
         if length > 0:
-            if length % 2 == 1: # can avoid some unsymmetric situation
+            if length % 2 == 1: # can avoid some unsmetric situation
                 return False
             stack.append(s[0]) # notice: be careful for null input!
             for i in xrange(1, length):
@@ -28,6 +28,28 @@ class Solution(object):
                         stack.pop()
                 else:
                     stack.append(s[i])
-        return stack == []
+        return not stack
 
-    def isValid(self, s):
+    def isValidGoogle(self, str): # google version
+        stack = []
+        for s in str:
+            if s == '(':
+                stack.append(s)
+            elif s == ')':
+                if stack:
+                    stack.pop()
+                else:
+                    return False
+        return not stack
+
+if __name__ == "__main__":
+    s = Solution()
+    print s.isValidGoogle("(google)rocks")
+    print s.isValidGoogle("(goo)(gle)()rocks")
+    print s.isValidGoogle("goo((gle))rocks")
+    print s.isValidGoogle("google((rocks)")
+    print s.isValidGoogle("google((rocks))))")
+
+
+
+
