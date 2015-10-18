7a9a7d6d14446d15bc099b492c49f31cd6ef7e26
chrishsu <chia-hao.hsu@aiesec.net>
2015-10-18 10:59:00 -0400

    finish maximumSubarrayII

--- lintCode/maximumSubarrayII.py
+++ lintCode/maximumSubarrayII.py
@@ -4,22 +4,27 @@ class Solution:
     @return: An integer denotes the sum of max two non-overlapping subarrays
     """
     def maxTwoSubArrays(self, nums):
-        min_idx = 0
-        for i in xrange(len(nums)):
-            if nums[i] < nums[min_idx]:
-                min_idx = i
-        if nums[min_idx] < 0:
-            return self.maxSubarray(nums[:min_idx]) + self.maxSubarray(nums[min_idx + 1:])
-        else:
-            return self.maxSubarray(nums)
-
-    def maxSubarray(self, nums):
-        curr_max = final_max = -9999
-        for n in nums:
+        # reference: http://hehejun.blogspot.com/2015/01/lintcodemaximum-subarray-ii.html
+        length = len(nums)
+        curr_max = -9999
+        # notice, here we store global maximum
+        # therefore, we should store very small value first
+        # to avoid the full negative elements cases.
+        left = [-9999 for i in xrange(length)]
+        for i in xrange(length - 1):
             if curr_max < 0:
                 curr_max = 0
-            curr_max += n
-            final_max = max(curr_max, final_max)
+            curr_max += nums[i]
+            left[i + 1] = max(left[i], curr_max)
+        # then, we combine local maximum for every position
+        # and find the final maximum at the same time
+        curr_max = final_max = -9999
+        for i in xrange(length - 1, 0, -1):
+            if curr_max < 0:
+                curr_max = 0
+            curr_max += nums[i]
+            if curr_max + left[i] > final_max:
+                final_max = curr_max + left[i]
         return final_max
 
 if __name__ == "__main__":
@@ -27,5 +32,3 @@ if __name__ == "__main__":
     print s.maxTwoSubArrays([1, 3, -1, 2, -1, 2])
     print s.maxTwoSubArrays([1, -99, -1, 2])
     print s.maxTwoSubArrays([1, 2, 3, 4])
-
-
