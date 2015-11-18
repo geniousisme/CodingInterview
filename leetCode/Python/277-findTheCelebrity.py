34dd2cd08437894116a8a4afd8dc57bca4668486
chrishsu <chia-hao.hsu@aiesec.net>
2015-11-18 00:25:15 -0500

    Finish find the celebrity

--- leetCode/Python/277-findTheCelebrity.py
+++ leetCode/Python/277-findTheCelebrity.py
@@ -1,3 +1,18 @@
+# Suppose you are at a party with n people (labeled from 0 to n - 1) and among them, there
+# may exist one celebrity. The definition of a celebrity is that all the other n - 1
+# people know him/her but he/she does not know any of them.
+# Now you want to find out who the celebrity is or verify that there is not one.
+# The only thing you are allowed to do is to ask questions like: "Hi, A. Do you know B?"
+# to get information of whether A knows B. You need to find out the celebrity (or verify there is not one)
+# by asking as few questions as possible (in the asymptotic sense).
+
+# You are given a helper function bool knows(a, b)
+# which tells you whether A knows B. Implement a function int findCelebrity(n),
+# your function should minimize the number of calls to knows.
+# Note: There will be exactly one celebrity if he/she is in the party.
+# Return the celebrity's label if there is a celebrity in the party.
+# If there is no celebrity, return -1.
+
 # Time:  O(n)
 # Space: O(1)
 #
@@ -21,7 +36,6 @@ class Solution(object):
                 candidate = i
         # Verify the candidate.
         for i in xrange(n):
-            if i != candidate and (knows(candidate, i) \
-                or not knows(i, candidate)):
+            if i != candidate and (knows(candidate, i) or not knows(i, candidate)):
                 return -1
-        return candidat
+        return candidate
