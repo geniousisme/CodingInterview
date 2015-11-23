c199a2ed3a5e92ebea7b7f747ba4af47e7aca891
chrishsu <chia-hao.hsu@aiesec.net>
2015-11-22 20:54:34 -0500

    Add combination mathematics solution

--- leetCode/Python/62-uniquePaths.py
+++ leetCode/Python/62-uniquePaths.py
@@ -1,7 +1,8 @@
-class Solution:
-    # @param {integer} m
-    # @param {integer} n
-    # @return {integer}
+class Solution1(object):
+    '''
+    Time:  O(m * n)
+    Space: O(m * n)
+    '''
     def uniquePaths(self, m, n):
         dp = [[1 for i in xrange(n)] for j in xrange(m)]
         for i in xrange(1, m):
@@ -9,9 +10,31 @@ class Solution:
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
         return dp[-1][-1]
 
-    def print_matrix(self, matrix):
-        for i in xrange(len(matrix)):
-            print ' '.join([str(n) for n in matrix[i]])
+class Solution2(object):
+    '''
+    Time:  O(m * n)
+    Space: O(n)
+    '''
+    def uniquePaths(self, m, n):
+        if n > m:
+            self.uniquePaths(n, m)
+        ways = [1] * n
+        # each time use one n sized array to present the latest level of matrix
+        for i in xrange(1, m):
+            for j in xrange(1, n):
+                ways[j] += ways[j - 1]
+        return ways[-1]
+
+from math import factorial
+class Solution(object):
+    '''
+    Time:  O(m + n)
+    Space: O(1)
+    '''
+    def uniquePaths(self, m, n):
+        if not m or not n:
+            return 0
+        return factorial(m + n - 2) / (factorial(n - 1) * factorial(m - 1))
 
 if __name__ == '__main__':
    s = Solution()
