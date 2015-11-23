3db51e108823ad18e98c68820a51cb13f8b117bb
chrishsu <chia-hao.hsu@aiesec.net>
2015-11-22 21:33:19 -0500

    Add questions and comments

--- leetCode/Python/246-StrobogrammaticNumber.py
+++ leetCode/Python/246-StrobogrammaticNumber.py
@@ -1,6 +1,15 @@
+# A strobogrammatic number is a number that looks the same
+# when rotated 180 degrees (looked at upside down).
+# Write a function to determine if a number is strobogrammatic.
+# The number is represented as a string.
+# For example, the numbers "69", "88", and "818" are all strobogrammatic.
+
+
 class Solution:
-    # @param {string} num
-    # @return {boolean}
+    '''
+    Time:  O(n)
+    Space: O(1)
+    '''
     def isStrobogrammatic(self, num):
         strobogram_dict = {'6':'9', '9':'6', '1':'1', '8':'8', '0':'0'}
         left = 0; right = len(num) - 1
