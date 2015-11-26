cb3c28f2802937098a3fa996d6715a94a948ce77
chrishsu <chia-hao.hsu@aiesec.net>
2015-11-26 16:28:22 -0500

    Finish zigzag iterator

---
+++ leetCode/Python/281-zigzagIterator.py
@@ -0,0 +1,32 @@
+# Given two 1d vectors,
+# implement an iterator to return their elements alternately.
+
+# For example, given two 1d vectors:
+# v1 = [1, 2]
+# v2 = [3, 4, 5, 6]
+# By calling next repeatedly until hasNext returns false,
+# the order of elements returned by next should be: [1, 3, 2, 4, 5, 6].
+
+# Follow up: What if you are given k 1d vectors?
+# How well can your code be extended to such cases?
+
+
+import collections
+class ZigzagIterator(object):
+
+    def __init__(self, v1, v2):
+        self.dq = collections.deque([(len(v), iter(v)) for v in (v1, v2) if v])
+
+    def next(self):
+        length, iterator = self.dq.popleft()
+        if length > 1:
+            self.dq.append((length - 1, iterator))
+        return next(iterator)
+
+    def hasNext(self):
+        return bool(self.dq)
+
+if __name__ == "__main__":
+    zi = ZigzagIterator([1, 2], [3, 4, 5, 6])
+    while zi.hasNext():
+        print zi.next()
