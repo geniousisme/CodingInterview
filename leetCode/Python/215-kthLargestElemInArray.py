526907a0af9291056612283c3443dd2687a304f8
chrishsu <chia-hao.hsu@aiesec.net>
2015-11-28 00:24:26 -0500

    Update with constant time complexity solution

--- leetCode/Python/215-kthLargestElemInArray.py
+++ leetCode/Python/215-kthLargestElemInArray.py
@@ -5,7 +5,10 @@ class Solution:
     # @param {integer} k
     # @return {integer}
     def findKthLargest(self, nums, k):
-        
+        '''
+        Time:  O(n)
+        Space: O(n)
+        '''
         pivot = random.choice(nums)
         nums1 = []; nums2 = []
         length = len(nums)
@@ -20,6 +23,28 @@ class Solution:
              return self.findKthLargest(nums2, k - (length - len(nums2)))
         else:
              return pivot
+
+class Solution2(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        pivot = nums[0]
+        tail = 0
+        for i in range(1, len(nums)):
+            if nums[i] > pivot:
+                tail += 1
+                nums[tail], nums[i] = nums[i], nums[tail]
+
+        nums[tail], nums[0] = nums[0], nums[tail]
+        if tail + 1 == k:
+            return pivot
+        elif tail + 1 < k:
+            return self.findKthLargest(nums[tail+1:], k - tail - 1)
+        else:
+            return self.findKthLargest(nums[:tail], k)  #excluding pivot
 
 if __name__ == '__main__':
    s = Solution()
