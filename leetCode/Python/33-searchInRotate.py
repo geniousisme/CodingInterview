ff90826ed29e96fa6d48bb4386677e215c7ed8f1
chrishsu <chia-hao.hsu@aiesec.net>
2015-11-28 11:09:30 -0500

    Upadte Solution

--- leetCode/Python/33-searchInRotate.py
+++ leetCode/Python/33-searchInRotate.py
@@ -1,7 +1,42 @@
-class Solution:
-    # @param {integer[]} nums
-    # @param {integer} target
-    # @return {integer}
+# Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+# (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+# You are given a target value to search.
+# If found in the array return its index, otherwise return -1.
+# You may assume no duplicate exists in the array.
+class Solution(object):
+    '''
+    Time:  O(logn)
+    Space: O(1)
+    '''
+    def search(self, nums, target):
+        if not nums:
+            return -1
+        start = 0; end = len(nums) - 1
+        while start + 1 < end:
+            mid = (start + end) / 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] >= nums[start]:
+                if nums[start] <= target <= nums[mid]:
+                    end = mid
+                else:
+                    start = mid
+            else:
+                if nums[mid] <= target <= nums[end]:
+                    start = mid
+                else:
+                    end = mid
+        if nums[start] == target:
+            return start
+        if nums[end] == target:
+            return end
+        return -1
+
+class Solution1(object):
+    '''
+    Time:  O(n)
+    Space: O(1)
+    '''
     def search(self, nums, target):
         length         = len(nums)
         pivot          = self.get_pivot(nums)
