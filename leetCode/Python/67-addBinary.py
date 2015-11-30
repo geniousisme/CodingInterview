da4df1a052bc0a15f2f3bfe35965de4431503150
chrishsu <chia-hao.hsu@aiesec.net>
2015-11-30 00:09:50 -0500

    Update add binary

--- leetCode/Python/67-addBinary.py
+++ leetCode/Python/67-addBinary.py
@@ -1,24 +1,57 @@
-class Solution:
-    # @param {string} a
-    # @param {string} b
-    # @return {string}
+# Given two binary strings, return their sum (also a binary string).
+# For example,
+# a = "11"
+# b = "1"
+# Return "100".
+
+class Solution1(object):
     def addBinary(self, a, b):
-        result_binary = str(int(a) + int(b))
-        result_binary = [int(b) for b in result_binary]
-        result_binary.insert(0, 0)
-        length        = len(result_binary)
-        carry         = 0
-        for i in xrange(length - 1, -1, -1):
-            result_binary[i] += carry
-            if result_binary[i] > 1: # only can be 2 or 3
-               result_binary[i] -= 2
-               carry = 1
-            else:
-               carry = 0
-        if result_binary[0] == 0:
-           result_binary = result_binary[1:]
-        return ''.join([str(rb) for rb in result_binary])
-                
+        """
+        Time:  O(n)
+        Space: O(n)
+        """
+        a = a[::-1]
+        b = b[::-1]
+        ai = bi = carry = 0; res = []
+        while ai < len(a) and bi < len(b):
+            curr = int(a[ai]) + int(b[bi]) + carry
+            carry = curr // 2
+            res.append(str(curr % 2))
+            ai += 1; bi += 1
+        while ai < len(a):
+            curr = int(a[ai]) + carry
+            carry = curr // 2
+            res.append(str(curr % 2))
+            ai += 1
+        while bi < len(b):
+            curr = int(b[bi]) + carry
+            carry = curr // 2
+            res.append(str(curr % 2))
+            bi += 1
+        if carry:
+            res.append(str(carry))
+        return ''.join(reversed(res))
+
+class Solution(object):
+    def addBinary(self, a, b):
+        '''
+        Time:  O(n)
+        Space: O(n)
+        '''
+        ai = len(a) - 1; bi = len(b) - 1; curr = carry = 0; res = ""
+        while ai >= 0 or bi >= 0:
+            if ai >= 0:
+                curr += int(a[ai])
+                ai -= 1
+            if bi >= 0:
+                curr += int(b[bi])
+                bi -= 1
+            curr += carry
+            carry = curr // 2
+            res += str(curr % 2)
+        if carry:
+            res += str(carry)
+        return res[::-1]
 
 if __name__ == '__main__':
    s = Solution()
