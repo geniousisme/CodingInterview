f005dbbc06939bde01259f5673f1887f7d02c982
chrishsu <chia-hao.hsu@aiesec.net>
2015-12-01 21:34:14 -0500

    Update shuffle again

--- CC150/shuffle.py
+++ CC150/shuffle.py
@@ -2,8 +2,8 @@ import random
 
 def shuffle(nums):
     for i in xrange(len(nums)):
-        replace = int(random.random() * (len(nums) - i) + i) # notice, idx cannot be float
-        nums[i], nums[replace] = nums[replace], nums[i]
+        replace = int(random.random() * (len(nums) - i) + i)
+        nums[replace], nums[i] = nums[i], nums[replace]
 
 if __name__ == "__main__":
     nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
