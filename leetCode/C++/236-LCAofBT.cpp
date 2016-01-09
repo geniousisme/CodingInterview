18d8244dd226e77ef63003994f80313267720be4
chrishsu <chia-hao.hsu@aiesec.net>
2016-01-09 15:06:34 +0800

    Update 236 & 237 C++ review

--- leetCode/C++/236-LCAofBT.cpp
+++ leetCode/C++/236-LCAofBT.cpp
@@ -11,19 +11,19 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-              if (root == nullptr) {  
-                  return nullptr;
-              };
-              if (root == p || root == q) {
-                  return root;
-              };
-              TreeNode *leftNode  = lowestCommonAncestor(root->left,  p, q);
-              TreeNode *rightNode = lowestCommonAncestor(root->right, p, q);
-              if (leftNode && rightNode) {
-                  return root;
-              };
-              return leftNode != nullptr ? leftNode : rightNode;
-    }   
+        if (!root)
+            return root;
+        if (root == p or root == q)
+            return root;
+
+        TreeNode* left_lca = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right_lca = lowestCommonAncestor(root->right, p, q);
+
+        if (left_lca && right_lca)
+            return root;
+
+        return left_lca ? left_lca : right_lca;
+    }
 };
 
 int main(void) {
