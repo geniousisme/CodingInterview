04d5b7f73f2581b94161814b2a99faa3b8c4a149
chrishsu <chia-hao.hsu@aiesec.net>
2016-01-10 11:20:30 +0800

    Update C++ review

--- leetCode/C++/278-findFirstBadVersion.cpp
+++ leetCode/C++/278-findFirstBadVersion.cpp
@@ -1,5 +1,5 @@
 // Forward declaration of isBadVersion API.
-// bool isBadVersion(int version);
+bool isBadVersion(int version);
 
 class Solution {
 public:
@@ -21,7 +21,7 @@ public:
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int start = 1, end = n; mid = 0;
+        int start = 1, end = n, mid = 0;
         while (start + 1 < end) {
             mid = start + (end - start) / 2;
             if (isBadVersion(mid))
