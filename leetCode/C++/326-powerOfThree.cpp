f51821d2ceb6d86fe987fe2b601bbfb9aff32fc8
chrishsu <chia-hao.hsu@aiesec.net>
2016-01-12 15:28:30 +0800

    Update 326-power of three C++ review

--- leetCode/C++/326-powerOfThree.cpp
+++ leetCode/C++/326-powerOfThree.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-class Solution {
+class Solution1 { // log calculation
 public:
     bool isPowerOfThree(int n) {
         if (n < 1)
@@ -10,3 +10,29 @@ public:
         return n == pow(3, round((log(n) / log(3))));
     }
 };
+
+class Solution2 { // iterative
+public:
+    bool isPowerOfThree(int n) {
+        if (n < 1)
+            return false;
+        while (n > 1) {
+            if (n % 3 != 0)
+                return false;
+            n /= 3;
+        }
+        return true;
+    }
+};
+
+class Solution { // recursive
+public:
+    bool isPowerOfThree(int n) {
+        if (n <= 0)
+            return false;
+        if (n == 1)
+            return true;
+        return n % 3 == 0 && isPowerOfThree(n / 3);
+    }
+};
+
