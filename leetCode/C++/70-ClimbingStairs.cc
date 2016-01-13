81da6d28111b8f41f90db8e79048cfe0c47efc65
chrishsu <chia-hao.hsu@aiesec.net>
2016-01-13 12:44:20 +0800

    Update 70 - climb stairs C++ review

--- leetCode/C++/70-ClimbingStairs.cc
+++ leetCode/C++/70-ClimbingStairs.cc
@@ -1,33 +1,61 @@
-#include "include/common.h"
+#include <iostream>
+#include <vector>
 
-class Solution {
+using namespace std;
+
+class Solution1 { // recursive solution
 public:
-    int recClimbStairs(int n) {
-        if (n == 1 || n == 0) {
-            return 1;
-        };
-        return climbStairs(n - 1) + climbStairs(n - 2);
-    }
-    int climbStairsI(int n) {
-        vector<int> res(2, 1);
-        if (n >= 2) {
-            for (int i = 2; i <= n; i++) {
-                 res.push_back(res[i - 1] + res[i - 2]);
-            }
-        };
-        return res[n];
-    }
     int climbStairs(int n) {
-        if (n == 0 || n == 1) {
+        /*
+            Time:  O(n)
+            Space: O(n)
+        */
+        if (n <= 1)
             return 1;
-        };
         return climbStairs(n - 1) + climbStairs(n - 2);
     }
 };
 
+class Solution2 { // iterative solution
+public:
+    int climbStairs(int n) {
+        /*
+            Time:  O(n)
+            Space: O(n)
+        */
+        vector<int> lookup {1, 1};
+        if (n <= 1)
+            return lookup[n];
+        for (int i = 2; i < n + 1; ++i)
+            lookup.push_back(lookup[i - 1] + lookup[i - 2]);
+        return lookup[n];
+    }
+};
+
+class Solution { // iterative solution with constant space
+public:
+    int climbStairs(int n) {
+        /*
+            Time:  O(n)
+            Space: O(1)
+        */
+        int prev = 1, prev_prev = 1, res = 0;
+        if (n <= 1)
+            return 1;
+        for (int i = 2; i < n + 1; ++i) {
+            res = prev + prev_prev;
+            prev_prev = prev;
+            prev = res;
+        }
+        return res;
+    }
+};
+
+
 int main(void) {
     Solution s;
-    for (int i = 0; i < 45; i++)  cout << s.climbStairs(i) << " ";
+    for (int i = 0; i < 45; i++)
+        cout << s.climbStairs(i) << " ";
     cout << endl;
     return 0;
 };
