b474f7bae3b68f65e4d6119f23945f0fd3bf37e1
chrishsu <chia-hao.hsu@aiesec.net>
2016-01-13 19:29:51 +0800

    Update 102 - binary tree level order traversal review

--- leetCode/C++/102-BinaryTreeLevelOrderTraversal.cc
+++ leetCode/C++/102-BinaryTreeLevelOrderTraversal.cc
@@ -1,40 +1,70 @@
-#include "include/common.h"
+#include <vector>
+#include <iostream>
+
+using namespace std;
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
  */
-class Solution {
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution1 { // DFS
 public:
-    vector<vector<int> > levelOrder(TreeNode* root) {
-                         vector<vector<int> > res;
-                         if (root) {
-                             levelOrderHelper(root, res, 1);
-                         };
-                         return res;
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        if (root)
+            level_order_helper(root, res, 1);
+        return res;
     }
 private:
-    void levelOrderHelper(TreeNode *root, vector<vector<int> >& res, int depth) {
-         if (root == nullptr) {
-             return;
-         };
-         if (res.size() < depth) {
-             vector<int> level;
-             res.push_back(level);
-         };
-         res[depth - 1].push_back(root->val);
-         levelOrderHelper(root->left,  res, depth + 1);
-         levelOrderHelper(root->right, res, depth + 1);
+    void level_order_helper(TreeNode* root, vector<vector<int>>& res, int depth) {
+        if (!root)
+            return;
+        if (depth > res.size()) {
+            vector<int> new_level;
+            res.push_back(new_level);
+        }
+        res[depth - 1].push_back(root->val);
+        level_order_helper(root->left, res, depth + 1);
+        level_order_helper(root->right, res, depth + 1);
+    }
+};
+
+class Solution { // BFS
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        vector<int> vals;
+        vector<TreeNode*> curr_level;
+        vector<TreeNode*> next_level;
+        if (root) {
+            curr_level.push_back(root);
+            while (!curr_level.empty()) {
+                TreeNode* top = curr_level.back();
+                curr_level.pop_back();
+                vals.push_back(top->val);
+                if (top->left)
+                    next_level.insert(next_level.begin(), top->left);
+                if (top->right)
+                    next_level.insert(next_level.begin(), top->right);
+                if (curr_level.empty()) {
+                    res.push_back(vals);
+                    curr_level = next_level;
+                    next_level.clear();
+                    vals.clear();
+                }
+            }
+        }
+        return res;
     }
 };
 
 int main(void) {
     Solution s;
-    Utils u;
     TreeNode root(1);
     TreeNode lnode(2);
     TreeNode rnode(3);
@@ -46,6 +76,12 @@ int main(void) {
     (&root)->right = &rnode;
     (&root)->left->left = &llnode;
     (&root)->right->right = &rrnode;
-    u.print2DIntVector(s.levelOrder(&root));
+    vector<vector<int>> level_orders = s.levelOrder(&root);
+    for (const auto &level: level_orders) {
+        cout << "[";
+        for (const auto &node: level)
+            cout << node << " ";
+        cout << "]" << endl;
+    }
     return 0;
 };
