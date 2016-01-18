3f4a284303cec924d092bdeb65e9e1ad5bb4a09e
chrishsu <chia-hao.hsu@aiesec.net>
2016-01-18 11:27:55 -0500

    Finish 141 - linked list cycle & 142 - linked list cycle II C++ review

--- leetCode/C++/141-LinkedListCycle.cc
+++ leetCode/C++/141-LinkedListCycle.cc
@@ -1,3 +1,10 @@
+/*
+Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+Can you solve it without using extra space?
+*/
+
 #include <iostream>
 
 using namespace std;
@@ -11,14 +18,13 @@ struct ListNode {
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-         ListNode *fast = head, *slow = head;
-         while (slow && fast && fast->next) {
-                slow = slow->next;
-                fast = fast->next->next;
-                if (slow == fast) {
-                   return true;
-                };
-         };
-         return false;
+        ListNode *fast = head, *slow = head;
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow)
+                return true;
+        }
+        return false;
     }
 };
--- leetCode/C++/142-LinkedListCycleII.cc
+++ leetCode/C++/142-LinkedListCycleII.cc
@@ -1,3 +1,13 @@
+/*
+Given a linked list, return the node where the cycle begins.
+If there is no cycle, return null.
+
+Note: Do not modify the linked list.
+
+Follow up:
+Can you solve it without using extra space?
+*/
+
 #include <iostream>
 
 using namespace std;
@@ -7,28 +17,30 @@ struct ListNode {
       ListNode *next;
       ListNode(int x) : val(x), next(NULL) {}
 };
+
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-             ListNode *fast = head, *slow = head;
-             bool met = false;
-             while (slow && fast && fast->next) {
-                    if (!met) {
-                        slow = slow->next;
-                        fast = fast->next->next;
-                        if (slow == fast) {
-                            met  = true;
-                            slow = head;
-                        };
-                    } else {
-                        while (slow != fast) {
-                               slow = slow->next;
-                               fast = fast->next;
-                        };
-                        return slow;
-                    };
-             };
-             return nullptr;
+        if (!head || !(head->next))
+            return nullptr;
+
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast)
+                break;
+        }
+
+        if (!fast || !(fast->next))
+            return nullptr;
+
+        slow = head;
+        while (fast != slow) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        return slow;
     }
 };
 int main(void) {
