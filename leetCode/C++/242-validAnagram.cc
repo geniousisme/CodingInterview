c5a56ab751fc0f06b6cf06f9068d21d0e1c6c235
chrishsu <chia-hao.hsu@aiesec.net>
2016-01-17 21:45:40 -0500

    Update 242 - valid anagram C++ review

--- leetCode/C++/242-validAnagram.cc
+++ leetCode/C++/242-validAnagram.cc
@@ -1,27 +1,51 @@
-#include "include/common.h"
+#include <algorithm>
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution1 {
+public:
+    bool isAnagram(string s, string t) {
+        /*
+            Time:  O(nlogn)
+            Space: O(1)
+        */
+         if (s.length() != t.length())
+             return false;
+         sort(s.begin(), s.end());
+         sort(t.begin(), t.end());
+         return s == t;
+    }
+};
 
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-         if (s.length() != t.length()) {
+        /*
+            Time:  O(n)
+            Space: O(n)
+        */
+         unordered_map<char, int> count;
+         if (s.length() != t.length())
              return false;
-         };
-         sort(s.begin(), s.end());
-         sort(t.begin(), t.end());
-         for(int i = 0; i < s.length(); i++) {
-             if (s[i] != t[i]) {
-                 return false;
-             };
-         };
+         for (const auto &c: s)
+            ++count[tolower(c)];
+         for (const auto &c: t) {
+            --count[tolower(c)];
+            if (count[tolower(c)] < 0)
+                return false;
+        }
          return true;
     }
 };
 
+
 int main(void) {
     Solution s;
     string test1 = "bdac", test2 = "abcd";
-    cout << s.isAnagram(test1, test2) << endl; 
+    cout << s.isAnagram(test1, test2) << endl;
     test1 = "car", test2 = "rat";
-    cout << s.isAnagram(test1, test2) << endl; 
+    cout << s.isAnagram(test1, test2) << endl;
     return 0;
 };
