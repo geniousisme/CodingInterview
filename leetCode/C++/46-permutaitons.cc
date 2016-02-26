02baf4aa94a0c4e45d63e01a1af72d0b2f209471
Chia-Hao Hsu <chia-haohsu@Chia-Haos-MacBook-Pro.local>
2016-02-25 22:00:08 -0500

    Finish the review of permutations

--- leetCode/C++/46-permutaitons.cc
+++ leetCode/C++/46-permutaitons.cc
@@ -1,32 +1,66 @@
-#include "include/common.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
 
 class Solution {
 public:
-    vector<vector<int> > permute(vector<int>& nums) {
-                         vector<vector<int> > res;
-                         if (!nums.empty()) {
-                             vector<int> perm, visited(nums.size(), 0);
-                             permuteHelper(res, visited, nums, perm, nums.size());
-                         };
-                         return res;
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        if (!nums.empty()) {
+            total_length = nums.size();
+            vector<int> perm;
+            vector<bool> visited(total_length, false);
+            permute_helper(nums, perm, res, visited);
+        }
+        return res;
     }
 private:
-    void permuteHelper(vector<vector<int> >& res, vector<int>& visited, vector<int>& nums, vector<int>& perm, int totalLen) {
-         if (perm.size() == totalLen) {
-             res.push_back(perm);
-             return;
-         };
-         for (int i = 0; i < nums.size(); i++) {
-              if (visited[i] == 0) {
-                  perm.push_back(nums[i]);
-                  visited[i] = 1;
-                  permuteHelper(res, visited, nums, perm, totalLen);
-                  visited[i] = 0;
-                  perm.pop_back();
-              };
-         };
-    }; 
+    int total_length = 0;
+    void permute_helper(vector<int>& nums, vector<int>& perm, vector<vector<int>>& res, vector<bool>& visited) {
+        if (perm.size() == total_length) {
+            res.push_back(perm);
+            return;
+        }
+        for (int i = 0; i < nums.size(); ++i) {
+            if (!visited[i]) {
+                visited[i] = true;
+                perm.push_back(nums[i]);
+                permute_helper(nums, perm, res, visited);
+                perm.pop_back();
+                visited[i] = false;
+            }
+        }
+    }
 };
+
+// class Solution {
+// public:
+//     vector<vector<int> > permute(vector<int>& nums) {
+//                          vector<vector<int> > res;
+//                          if (!nums.empty()) {
+//                              vector<int> perm, visited(nums.size(), 0);
+//                              permuteHelper(res, visited, nums, perm, nums.size());
+//                          };
+//                          return res;
+//     }
+// private:
+//     void permuteHelper(vector<vector<int> >& res, vector<int>& visited, vector<int>& nums, vector<int>& perm, int totalLen) {
+//          if (perm.size() == totalLen) {
+//              res.push_back(perm);
+//              return;
+//          };
+//          for (int i = 0; i < nums.size(); i++) {
+//               if (visited[i] == 0) {
+//                   perm.push_back(nums[i]);
+//                   visited[i] = 1;
+//                   permuteHelper(res, visited, nums, perm, totalLen);
+//                   visited[i] = 0;
+//                   perm.pop_back();
+//               };
+//          };
+//     }; 
+// };
 
 int main(void) {
     Solution s;
