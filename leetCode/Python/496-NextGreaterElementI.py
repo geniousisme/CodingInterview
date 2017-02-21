74bf80d038fd2dd790ed4d855c6935968704bf42
Chris Hsu <chia-hao.hsu@aiesec.net>
2017-02-22 00:00:08 +0800

    Finish NextGreaterElementI

---
+++ leetCode/Python/496-NextGreaterElementI.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def nextGreaterElement(self, findNums, nums):
+        """
+        :type findNums: List[int]
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        next_greater_dict = {}
+        stack = []
+        for n in nums:
+            while stack and stack[-1] < n:
+                next_greater_dict[stack.pop()] = n
+            stack.append(n)
+        return [next_greater_dict.get(n, -1) for n in nums]
